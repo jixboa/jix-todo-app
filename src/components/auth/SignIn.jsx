2bfabff209f90ef3526eace1217a95c62e5205b7
Aboagye Gyamfi <gyesboa@gmail.com>
2022-04-27 06:37:01 +0000

    signIn

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
+// import Backdrop from '@mui/material/Backdrop'
 
 import { signIn } from "../../store/actions/authAction";
 
-import { Typography, TextField, Button } from "@material-ui/core";
+import {
+  Typography,
+  TextField,
+  Button,
+  Backdrop,
+  CircularProgress,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyle = makeStyles({
@@ -45,7 +52,16 @@ const SignIn = () => {
     });
   };
 
+  // <div>
+  //   <Backdrop
+  //     sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+  //     open={true}>
+  //     <CircularProgress color="inherit" />
+  //   </Backdrop>
+  // </div>
+
   if (auth._id) return <Navigate to="/" />;
+
   return (
     <>
       <form
